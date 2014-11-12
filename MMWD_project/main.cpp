a16aa81541d7b6e88875c1b22f848f9664016344
lukmar993 <lukmar993@gmail.com>
2014-11-12 15:35:37 +0100

    main

---
+++ MMWD_project/main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: lukasz
+ *
+ * Created on 12 listopad 2014, 11:30
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
