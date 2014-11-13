62ffe3865f8e4902d14954fc254db62fdd4a10c7
Lukasynex <lukmar993@gmail.com>
2014-11-13 13:05:35 +0100

    dodane funkcje, usunięte pola z javy

--- MMWD_project/Magazyn.cpp
+++ MMWD_project/Magazyn.cpp
@@ -1,45 +1,99 @@
-/* 
- * File:   Magazyn.cpp
- * Author: lukasz
- * 
- * Created on 12 listopad 2014, 11:55
- */
-
-#include "Magazyn.h"
 #include "Produkt.h"
 #include "Droga.h"
-Magazyn::Magazyn() {
-}
 
-Magazyn::Magazyn(const Magazyn& orig) {
-}
+#ifndef MAGAZYN_H
+#define	MAGAZYN_H
 
-Magazyn::~Magazyn() {
-}
 
-    //public Produkt[] produkt = new Produkt[MAX_LICZBA_PRODOKTOW];
-    
-    private int liczba_prodoktow;
-    private final int[] ilosc_produktu = new int[MAX_LICZBA_PRODOKTOW];
-    private final float[] cena_produktu = new float[MAX_LICZBA_PRODOKTOW];
-    private final int[] zapas_produktow = new int[MAX_LICZBA_PRODOKTOW];
-    private final float[] droga_do_magazynu = new float[MAX_LICZBA_MAGAZYNOW];
-    public Magazyn(int id){
-        ID = id;
-    }
-    
 
-    public int ZwrocZapasProduktu(int j){
-        return zapas_produktow[j];
+class Magazyn {
+private:
+    int ID;
+    Produkt** produkty;
+    int liczba_produktow;
+    double* droga_do_magazynu;
+
+    int dlugosc(int* wsk){
+    	int i = 0;
+    	while(wsk[i++])
+    		;
+    	return i;
     }
 
-    public void UstalDrogiDoInnychMagazynow(float[][] macierz_drog){
-        for(int j = 0; j < MAX_LICZBA_MAGAZYNOW;j++){
-            if(j==ID)
-                droga_do_magazynu[j] = ZakazanaWartoscDrogi;
-            else
-                droga_do_magazynu[j] = macierz_drog[ID][j];
+public:
+    static const int MAX_LICZBA_PRODUKTOW = 5;
+    static const int MAX_LICZBA_MAGAZYNOW = 5;
+    static const int MAX_ILOSC_DANEGO_PRODUKTU = 20;
+    static int zapotrzebowanie_na_produkty[MAX_LICZBA_PRODUKTOW];
+
+    Magazyn(int id, int liczba_produktow, double* wektor_drog){
+    	ID=id;
+    	droga_do_magazynu = new double[MAX_LICZBA_MAGAZYNOW];
+        this->liczba_produktow = liczba_produktow;
+        produkty = new Produkt*[liczba_produktow];
+        for(int i=0;i<liczba_produktow;i++)
+        	produkty[i] = new Produkt();
+
+        for(int i=0;i<MAX_LICZBA_MAGAZYNOW;i++){
+        	droga_do_magazynu[i] = wektor_drog[i];
         }
+        //"blokujemy" drogę do tego sklepu:
+        droga_do_magazynu[ID] = Droga::ZakazanaWartoscDrogi;
+    }
+    void UstalIloscProduktow(int iloscProduktow){
+    	liczba_produktow = iloscProduktow;
+    	produkty = new Produkt[liczba_produktow];
+    }
+    void UstalID(int i){ID = i;}
+    int PobierzID() const{
+    	return ID;
+    }
+
+    void UstalZapotrzebowanie(int* zapotrzebowanie){
+    	for(int i = 0; i< dlugosc(zapotrzebowanie) ;i++)
+    		zapotrzebowanie_na_produkty[i] = zapotrzebowanie[i];
+    }
+    bool Zapotrzebowanie(){
+    	for(int i = 0; i< dlugosc(zapotrzebowanie_na_produkty) ;i++)
+        		if(zapotrzebowanie_na_produkty[i]>0){
+        			return true;
+        		}
+    	return false;
+    }
+
+    void UstalCeneProduktu(int indeksProduktu, double cena){
+    	if(indeksProduktu<liczba_produktow)
+    		produkty[indeksProduktu]->ustalCene(cena);
+    }
+    void UstalDrogiDoInnychMagazynow(double** macierz_drog){
+		for(int j = 0; j < MAX_LICZBA_MAGAZYNOW;j++){
+			if(j==ID)
+				droga_do_magazynu[j] = Droga::ZakazanaWartoscDrogi;
+			else
+				droga_do_magazynu[j] = macierz_drog[ID][j];
+		}
+	}
+	int ZwrocLiczbeProduktow(){
+            return liczba_produktow;
+    }
+	double ZwrocCeneProduktu(int indeksProduktu){
+		return produkty[indeksProduktu]->Cena();
+	}
+	double ZwrocIloscProduktu(int indeksProduktu){
+		return produkty[indeksProduktu]->Ilosc();
+
+	}
+	//TODO: zrobić funkcję
+//	void KupProdukt(int indeksPoroduktu, int ilosc){
+//		int kupiono = produkty[indeksPoroduktu].KupProdukt(ilosc);
+//	}
+	double DlugoscPomiedzy(int magazyn){
+	        return droga_do_magazynu[magazyn];
+	    }
+
+    virtual ~Magazyn(){
+    	delete [] droga_do_magazynu;
+    	delete[] produkty;
     }
 
 };
